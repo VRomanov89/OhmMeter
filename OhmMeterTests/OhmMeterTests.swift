ffb4b7e14aefb7b393fa0e6296136529c5adec7b
Vladimir Romanov <vladromweb@gmail.com>
2015-10-21 21:44:14 -0400

    Initial Commit

---
+++ OhmMeterTests/OhmMeterTests.swift
@@ -0,0 +1,36 @@
+//
+//  OhmMeterTests.swift
+//  OhmMeterTests
+//
+//  Created by Volodymyr Romanov on 10/21/15.
+//  Copyright © 2015 EEEnthusiast. All rights reserved.
+//
+
+import XCTest
+@testable import OhmMeter
+
+class OhmMeterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
